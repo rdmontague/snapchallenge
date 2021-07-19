1fb86f7b2d168f345302daf34cffaff742a04995
Regina Montague <rmontague@cnm.edu>
2021-07-19 08:51:16 -0600

    push snap challenge

---
+++ snap-03/more-functions-01.js
@@ -0,0 +1,10 @@
+// Write a function in JavaScript that converts weights to grams. The function should take two arguments. The first argument will be a number, the second will be an abbreviation for a unit of weight, either "lbs", "oz", "kg", "g", or "mg". Based on the second argument, convert the number to grams and return it.
+
+const convertToGrams = (weight, units) => {
+    switch (units) {
+        case 'lbs':
+            const grams weight * 454
+            break
+        case
+    }
+}
